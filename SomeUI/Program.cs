09492a39cb1f0cb8efbbd0480fb4941b73c33a54
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-05-31 11:13:37 +0700

    add db context and run first migration

--- SomeUI/Program.cs
+++ SomeUI/Program.cs
@@ -2,7 +2,9 @@
 {
     class Program
     {
+#pragma warning disable IDE0060 // Remove unused parameter
         static void Main(string[] args)
+#pragma warning restore IDE0060 // Remove unused parameter
         {
         }
     }
