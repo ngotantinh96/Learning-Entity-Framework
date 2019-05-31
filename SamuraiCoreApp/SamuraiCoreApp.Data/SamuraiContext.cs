8973b8e44d6e6668800b3b2fcd88ae5560d4565a
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-05-31 11:40:01 +0700

    set up source for .net core and run first migration

---
+++ SamuraiCoreApp/SamuraiCoreApp.Data/SamuraiContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiCoreApp.Domain;
+
+namespace SamuraiCoreApp.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public SamuraiContext(DbContextOptions<SamuraiContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder
+        //        .UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=SamuraiAppData;Trusted_Connection=True;");
+        //}
+    }
+}
